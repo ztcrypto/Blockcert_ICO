4a5c13b83f6f194815b0606787619213fd00601b
Jay Sanchez <jay.sanchez@intelligentimagingsystems.com>
2020-01-03 22:43:15 -0800

    saving config and settings

--- truffle-config.js
+++ truffle-config.js
@@ -1,26 +1,74 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "flee sadness churn mixture harbor hurry helmet grid valve frame seat voice";
+
 module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      gas: 2816044,
-      network_id: "*", // Match any network id
-      gasPrice: 200000000000
-    },
-    testnet: {
-      from: "0xf8e5a2e7a7aa1148ab36dac0e5088a86e336ce79",
-      host: "94.130.35.43",
-      port: 6082,
-      gas: 2816044,
-      network_id: "*" // Match any network id
-    }
-  },
-  mocha: {
-    useColors: true
-  },
-  compilers: {
-    solc: {
-      version: "0.4.24"
-    }
-  }
+	networks: {
+		bcbc_testnet: {
+			provider: function() {
+				return new HDWalletProvider(mnemonic,"http://node2.blockcerts.com", 0);
+			},
+			network_id: "*",
+			gas: 4000000,
+			from: "0xdf9c0a954675a59aca0d2bec21be8a8395ae13cb"
+		},
+		truffle_develop: {
+			host: "127.0.0.1",
+			port: 9545,
+			gas: 2816044,
+			network_id: "*", // Match any network id
+			gasPrice: 200000000000
+		},
+		infura_ropsten: {
+			provider: function() {
+				return new HDWalletProvider(mnemonic,"https://ropsten.infura.io/v3/71cd22f7eea74abc8a7b4fafabbfffea", 0);
+			},
+			network_id: 3,
+			gas: 4698712,
+			gasPrice: 10000000000
+		},
+		ganache_cli: {
+			host: "127.0.0.1",
+			port: 8545,
+			gas: 2816044,
+			network_id: "*", // Match any network id
+			gasPrice: 200000000000
+		},
+		ganache_ui: {
+			host: "127.0.0.1",
+			port: 7545,
+			gas: 2816044,
+			network_id: "*", // Match any network id
+			gasPrice: 200000000000
+			//from: 0x6472A63Da4581Dd9090faF7B92C09282b94a06EA
+		},
+		AltCoinClient_XYZ: {
+			host: "127.0.0.1",
+			port: 7545,
+			gas: 2816044,
+			network_id: "*", // Match any network id
+			gasPrice: 200000000000,
+			from:  0x3f223D8681eAf9b2EaAf0006E037d49D069B9277
+		},
+		testnet: {
+			from: "0xf8e5a2e7a7aa1148ab36dac0e5088a86e336ce79",
+			host: "94.130.35.43",
+			port: 6082,
+			gas: 2816044,
+			network_id: "*" // Match any network id
+		}
+	},
+	mocha: {
+		useColors: true
+	},
+	compilers: {
+		solc: {
+		   version: "^0.4.24",    // Fetch exact version from solc-bin (default: truffle's version)
+		  settings: {          // See the solidity docs for advice about optimization and evmVersion
+		  	optimizer: {
+		  		enabled: false,
+		  		runs: 200
+		  	},
+		  }
+		}
+	}
 };
